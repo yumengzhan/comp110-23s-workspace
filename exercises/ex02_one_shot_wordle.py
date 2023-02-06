f7932b6d3b10f68c9c94a7ab4bd1d243e2a1e88d
Selina Zhang <yumengz@live.unc.edu>
2023-02-06 15:34:19 -0500

    Progress on exercise2

--- exercises/ex02_one_shot_wordle.py
+++ exercises/ex02_one_shot_wordle.py
@@ -1,45 +1,44 @@
 """EX01 - One-Shot Wordle - Loops!"""
 __author__ = "730619940"
 
-SECRET: str = "python"
-length: int = len(SECRET)
+secret: str = "python"
+length: int = len(secret)
 guess: str = input(f"What is your {length}-letter guess? ")
 
-while (len(guess) != len(SECRET)):  # The length of the guess is not equal to that of the secret word"
+while (len(guess) != len(secret)):  # The length of the guess is not equal to that of the secret word"
     guess = input(f"That was not {length} letters! Try again: ")
 
 playing: bool = True
 
-WHITE_BOX: str = "\U00002B1C"
-GREEN_BOX: str = "\U0001F7E9"
-YELLOW_BOX: str = "\U0001F7E8"
+white_box: str = "\U00002B1C"
+green_box: str = "\U0001F7E9"
+yellow_box: str = "\U0001F7E8"
 
 guess_idx: int = 0
 
 stored_variable: str = ""
 
-
 while playing:
     # Green Box if the character exists and in the right position
-    while guess_idx < len(SECRET):
-        if (guess[guess_idx] == SECRET[guess_idx]):
-            stored_variable = stored_variable + GREEN_BOX
+    while guess_idx < len(secret):
+        if (guess[guess_idx] == secret[guess_idx]):
+            stored_variable = stored_variable + green_box
         # Yellow Box if the character exists in the secret word but in wrong position
         else:
-            Character_exists: bool = False
-            Alternate_index: int = 0
-            while (Character_exists is False) and (Alternate_index < len(SECRET)):
-                if (SECRET[Alternate_index] == guess[guess_idx]):
-                    Character_exists = True    
+            character_exists: bool = False
+            alternate_index: int = 0
+            while (character_exists is False) and (alternate_index < len(secret)):
+                if (secret[alternate_index] == guess[guess_idx]):
+                    character_exists = True    
                 else:       
-                    Alternate_index = Alternate_index + 1        
-            if (Character_exists is True):
-                stored_variable = stored_variable + YELLOW_BOX
+                    alternate_index = alternate_index + 1        
+            if (character_exists is True):
+                stored_variable = stored_variable + yellow_box
             else:  # White box if the character does not exist
-                stored_variable = stored_variable + WHITE_BOX
+                stored_variable = stored_variable + white_box
         guess_idx = guess_idx + 1
     print(stored_variable)
-    if (guess != SECRET):
+    if (guess != secret):
         print("Not quite. Play again soon!")
     else:
         print("Woo! You got it!")
