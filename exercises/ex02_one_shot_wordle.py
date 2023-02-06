5a9b90d5e262104d936b446ce6b5d72eec99af83
Selina Zhang <yumengz@live.unc.edu>
2023-02-06 15:07:31 -0500

    Progress on Exercise 2

--- exercises/ex02_one_shot_wordle.py
+++ exercises/ex02_one_shot_wordle.py
@@ -2,50 +2,45 @@
 __author__ = "730619940"
 
 SECRET: str = "python"
-length: str= len(SECRET)
-guess: str = input(f"What is your {length} -letter guess? ")
-if (len(guess) != len(SECRET)):
-        guess = input(f"That was not {length} letters! Try again: ")
+length: int = len(SECRET)
+guess: str = input(f"What is your {length}-letter guess? ")
+
+while (len(guess) != len(SECRET)):
+    guess = input(f"That was not {length} letters! Try again: ")
+
 playing: bool = True
 
 WHITE_BOX: str = "\U00002B1C"
 GREEN_BOX: str = "\U0001F7E9"
 YELLOW_BOX: str = "\U0001F7E8"
 
-guess_idx: int=0
+guess_idx: int = 0
 
-
-
-stored_variable: str=""
+stored_variable: str = ""
 
 
 while playing:
     # Green Box
     while guess_idx < len(SECRET):
-        if(guess[guess_idx]==SECRET[guess_idx]):
-            stored_variable=stored_variable + GREEN_BOX
+        if (guess[guess_idx] == SECRET[guess_idx]):
+            stored_variable = stored_variable + GREEN_BOX
         # Yellow Box
         else:
-            Character_exists: bool=False
-            Alternate_index:int=0
-            while (Character_exists is False) and (Alternate_index<len(SECRET)):
+            Character_exists: bool = False
+            Alternate_index:int = 0
+            while (Character_exists is False) and (Alternate_index < len(SECRET)):
                 if (SECRET[Alternate_index] == guess[guess_idx]):
-                    Character_exists=True    
+                    Character_exists = True    
                 else:       
-                    Alternate_index=Alternate_index+1        
-            if(Character_exists is True):
-                stored_variable=stored_variable + YELLOW_BOX
+                    Alternate_index = Alternate_index+1        
+            if (Character_exists is True):
+                stored_variable = stored_variable + YELLOW_BOX
             else:
-                stored_variable=stored_variable + WHITE_BOX
-        guess_idx = guess_idx+1
+                stored_variable = stored_variable + WHITE_BOX
+        guess_idx = guess_idx + 1
     print(stored_variable)
     if (guess != SECRET):
         print("Not quite. Play again soon!")
     else:
         print("Woo! You got it!")
     playing = False
-
-    
-
-    
-
