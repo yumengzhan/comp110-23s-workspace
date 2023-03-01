b57ebbcf1484b17b05d2ed770dd2f4751240f8c0
Selina Zhang <yumengz@live.unc.edu>
2023-02-28 20:12:05 -0500

    Progress on Exercise

---
+++ exercises/ex04_utils.py
@@ -0,0 +1,36 @@
+"""EX04 - utils"""
+__author__ = "730619940"
+
+def all(numbers: list[int],num1:int) ->bool:
+    length: int=len(numbers)
+    i: int=0
+    while i<=length-1:
+        if numbers[i]!=num1:
+            return False
+        i+=1
+    return True
+
+def max(input: list[int]) ->int:
+    if len(input) == 0:
+        raise ValueError("max() arg is an empty List")
+    length: int=len(input)
+    i: int=0
+    maxnum: int=0
+    while i<=length-1:
+        if input[i]>maxnum:
+            maxnum=input[i]
+        i+=1 
+    return maxnum
+
+def is_equal(list1:list[int],list2:list[int]) ->bool:
+    if len(list1)!=len(list2):
+        return False
+    idx1:int=0
+    while idx1<len(list2):
+        if list1[idx1]!=list2[idx1]:
+            return False
+        idx1+=1
+    return True
+    
+
+
