7f21714aa31fb17ec4b254b9ea9c38081faadd94
Selina Zhang <yumengz@live.unc.edu>
2023-03-01 14:30:09 -0500

    progress on exercise

--- exercises/ex04_utils.py
+++ exercises/ex04_utils.py
@@ -3,6 +3,7 @@ __author__ = "730619940"
 
 
 def all(numbers: list[int], num1: int) -> bool:
+    """Testing if all the integers in the list are the same"""
     length: int = len(numbers)
     i: int = 0
     if len(numbers) == 0:
@@ -15,26 +16,25 @@ def all(numbers: list[int], num1: int) -> bool:
 
 
 def max(input: list[int]) -> int:
+    """Find the maximum integer in the list"""
     if len(input) == 0:
         raise ValueError("max() arg is an empty List")
     length: int = len(input)
     i: int = 1
-    maxnum:int = input[0]
+    maxnum: int = input[0]
     while i <= length - 1:
         if input[i] > maxnum:
             maxnum = input[i]
         i += 1 
     return maxnum
 
-def is_equal(list1:list[int],list2:list[int]) -> bool:
+def is_equal(list1: list[int], list2: list[int]) -> bool:
+    """Testing if two lists are the same"""
     if len(list1) != len(list2):
         return False
-    idx1:int = 0
+    idx1: int = 0
     while idx1 < len(list2):
         if list1[idx1] != list2[idx1]:
             return False
         idx1 += 1
     return True
-    
-
-
